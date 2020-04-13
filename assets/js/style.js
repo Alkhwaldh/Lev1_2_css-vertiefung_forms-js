781f447ea722a8e3395de85435e28891da30321b
Alkhwaldh <khwaldh89@gamil.com>
2020-04-13 12:19:46 +0200

    lev1_2 Forms JS

---
+++ assets/js/style.js
@@ -0,0 +1,8 @@
+function berechnen() {
+    /*  */
+const dasAlter =document.getElementById ("geburt").value
+const ergebniss = 2020 - dasAlter ;
+
+document.getElementById ("berechnungsErgebiss").innerText= ergebniss
+
+}
